7fed0d55edbe1ce78b86de0850115e93f9850585
Mitch <mitchell.hayen@gmail.com>
2022-06-05 12:27:45 -0500

    inti

---
+++ pages/eom.js
@@ -0,0 +1,11 @@
+export const eom = () => {
+    return(
+        <div className='page-container'>
+            <div>
+                <h1>Employee of the Month</h1>
+            </div>
+        </div>
+    );
+};
+
+export default eom;
